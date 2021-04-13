25e4f42923736e6549ca0240d0997700fd88c06e
Austin Farwell <austinmfarwell@gmail.com>
2021-04-13 15:24:57 -0700

    added temp css with back ground, fixed charzard picture, added height, xp, etc

--- Charizard/charizard.js
+++ Charizard/charizard.js
@@ -74,3 +74,138 @@ let input = `{
     "weight": 905
 }`
 
+let superObj = JSON.parse(input)
+let header = document.getElementById('header')
+let secton = document.getElementById('section')
+
+init(input)
+function init(input) {
+    createHeader(superObj)
+    createSection(superObj)
+}
+
+function createHeader(obj) {
+    const _img = document.createElement('img')
+    _img.src = "charzard.png"
+    header.appendChild(_img)
+}
+
+function createSection(obj) {
+    
+    //abilities
+    const _abilities = obj['abilities']
+    const _abilitiesContainer = document.createElement('div')
+    let title = document.createElement('h5')
+    title.textContent = 'Abilities'
+    _abilitiesContainer.appendChild(title)
+    for (a in _abilities){
+        const _h2 = document.createElement('h2')
+        
+        
+        _h2.textContent = _abilities[a]['name']
+
+        section.appendChild(_abilitiesContainer)
+        
+        _abilitiesContainer.appendChild(_h2)
+    }
+
+
+
+
+    //base_xp
+    const _baseXP = obj['base_experience']
+    const _baseXPContainer = document.createElement('div')
+
+    function createXP(){
+        const _h2 = document.createElement('h2')
+        _h2.textContent = `Base XP: ${_baseXP}`
+        section.appendChild(_baseXPContainer)
+        _baseXPContainer.appendChild(_h2)
+    }
+    createXP()
+
+    //height
+    const _height = obj['height']
+    const _heightContainer = document.createElement('div')
+    function createHeight(){
+        const _h2 = document.createElement('h2')
+        _h2.textContent = `Height: ${_height}`
+        section.appendChild(_heightContainer)
+        _baseXPContainer.appendChild(_h2)
+    }
+    createHeight()
+
+
+    // const _baseXP = obj['base_experience']
+    // const _baseXPContainer = document.createElement('div')
+    // function createXP(){
+    //     const _h2 = document.createElement('h2')
+    //     _h2.textContent = `Base XP: ${_baseXP}`
+    //     section.appendChild(_baseXPContainer)
+    //     _baseXPContainer.appendChild(_h2)
+    // }
+    // createXP()
+
+
+    // const _baseXP = obj['base_experience']
+    // const _baseXPContainer = document.createElement('div')
+    // function createXP(){
+    //     const _h2 = document.createElement('h2')
+    //     _h2.textContent = `Base XP: ${_baseXP}`
+    //     section.appendChild(_baseXPContainer)
+    //     _baseXPContainer.appendChild(_h2)
+    // }
+    // createXP()
+
+
+
+    // const _baseXP = obj['base_experience']
+    // const _baseXPContainer = document.createElement('div')
+    // function createXP(){
+    //     const _h2 = document.createElement('h2')
+    //     _h2.textContent = `Base XP: ${_baseXP}`
+    //     section.appendChild(_baseXPContainer)
+    //     _baseXPContainer.appendChild(_h2)
+    // }
+    // createXP()
+
+    // const _baseXP = obj['base_experience']
+    // const _baseXPContainer = document.createElement('div')
+    // function createXP(){
+    //     const _h2 = document.createElement('h2')
+    //     _h2.textContent = `Base XP: ${_baseXP}`
+    //     section.appendChild(_baseXPContainer)
+    //     _baseXPContainer.appendChild(_h2)
+    // }
+    // createXP()
+
+
+
+
+
+    //type
+    const _types = obj['types']
+    const _typeContainer = document.createElement('div')
+    for (j in _types) {
+        const _type = _types[j]['type']
+
+        
+        const _h2 = document.createElement('h2')
+        const _p = document.createElement('p')
+
+        _typeContainer.class = "card"
+        _h2.class = "top"
+        _p.class = "bottom"
+        for (i in _type) {
+            _h2.textContent = _type['name']
+        }
+
+
+
+        section.appendChild(_typeContainer)
+        _typeContainer.appendChild(_h2)
+        _typeContainer.appendChild(_p)
+        //end First card
+
+    }
+}
